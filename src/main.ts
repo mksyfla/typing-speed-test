ba548e2fe6e3318c58c839281cc8a4a979e1d63d
Kasyfil <mksyfla@gmail.com>
2026-06-28 10:23:19 +0700

    Add Header Components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import { header } from "./component/header";
 import "./styles.css";
 
 const app = document.getElementById("app") as HTMLDivElement;
 
-app.append();
+app.addEventListener("resize", () => {
+    console.log(window.innerWidth);
+});
+
+app.append(header());
